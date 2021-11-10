72ea7e10846c8c3e52fd394a091013278020676b
Grigor_Tumanyan <80620982+GrigorTumanyan@users.noreply.github.com>
2021-11-10 23:41:40 +0400

    added generic in classes change parameter in method fieldValidator added Message class and message static method for errors

---
+++ src/annotations/validateAnnotations/Messages.java
@@ -0,0 +1,10 @@
+package annotations.validateAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Messages {
+    public static List<String> messages = new ArrayList<>();
+
+
+}
